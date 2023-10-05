9908c73dd6bae17a7308e3bbd32a39099e76b44f
FahimmalAisyah <fahimmal.sasad@gmail.com>
2023-10-06 00:21:57 +0700

    coment crudsequelize

--- controller/productController.js
+++ controller/productController.js
@@ -3,7 +3,7 @@ const { Product } = require("../models");
 const findProducts = async (req, res) => {
     const { name, price, stock } = req.body;
     try {
-        const products = await Product.findAll(); // Menggunakan 'Product' (bukan 'Products') untuk mengambil data dari model
+        const products = await Product.findAll(); // menggunakan 'Product' (bukan 'Products') untuk mengambil data dari model
 
         res.status(200).json({
             status: "Success",
@@ -14,11 +14,11 @@ const findProducts = async (req, res) => {
     } catch (err) {
         res.status(400).json({
             status: "Failed",
-            message: err.message, // Menambahkan koma setelah "Failed"
+            message: err.message, 
         });
     }
 };
 
 module.exports = {
-    findProducts, // Menggunakan 'findProducts' (bukan 'createProduct')
+    findProducts, 
 };
